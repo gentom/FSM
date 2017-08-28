86eaefad35efd22d0ab9775b0acf27ef54ea6fd7
gentom <gento.m.developer@gmail.com>
2017-08-29 00:27:17 +0900

    commit

--- sm/sm.py
+++ sm/sm.py
@@ -4,12 +4,7 @@ from random import randint, random, choice
 
 # Finite State Machine. Work In Progress.
 
-class State:
-    def __init__(self, state):
-        self.state = state
-
-
-class Machine():
+class StateMachine:
     def __init__(self, states):
         self.states = states
         self.state_num = len(states)
@@ -17,9 +12,6 @@ class Machine():
 
     def get_current(self):
         return self.current_state
-
-    def simulator(self):
-        pass
 
     def state_changer(self, trigger):
         pass
