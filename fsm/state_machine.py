229fb7cdcbd96b20a674bbd332920adebbf0be78
gentom <gento.m.developer@gmail.com>
2017-08-28 23:56:12 +0900

    commit

---
+++ fsm/state_machine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+from random import randint, random, choice
+
+# Finite State Machine. Work In Progress.
+class StateMachine():
+    def __init__(self, state):
+        self.state = state
+        self.state_num = len(state)
+        self.current_state = state[0]
+
+    def get_current(self):
+        return self.current_state
+
+    def simulator(self):
+        pass
+
+    def state_changer(self, trigger):
+        pass
