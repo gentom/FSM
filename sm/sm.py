b09e06c2f215a30198e3561cec9b8d26aaa020dc
gentom <gento.m.developer@gmail.com>
2017-08-29 23:16:34 +0900

    commit

--- sm/sm.py
+++ sm/sm.py
@@ -21,3 +21,19 @@ class StateMachine:
             if rand < weight:
                 return state
         return None
+
+def state1():
+    print('State1: state1() is called')
+
+def state2():
+    print('State2: state2() is called')
+
+def state3():
+    print('State3: state3() is called')
+
+if __name__ == '__main__':
+    states = ['state1','state2','state3']
+    t_matrix = [0.6,0.2,0.2]
+    sm = StateMachine(states,t_matrix)
+    for i in range(10):
+        eval(sm.transitioner())()
